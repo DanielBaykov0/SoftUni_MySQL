e2b393bb46515227af9ad8bcccce255888fa6e4e
Daniel Baykov <99421433+DanielBaykov0@users.noreply.github.com>
2022-12-16 11:48:18 +0200

    update

---
+++ Exercises/Data Aggregation - Exercise.sql
@@ -0,0 +1,215 @@
+#1.Records' Count
+SELECT 
+    COUNT(id) AS `count`
+FROM
+    wizzard_deposits;
+
+#2.Longest Magic Wand
+SELECT 
+    magic_wand_size AS `longest_magic_wand`
+FROM
+    wizzard_deposits
+ORDER BY magic_wand_size DESC
+LIMIT 1;
+
+#3.Longest Magic Wand Per Deposit Groups
+SELECT 
+    deposit_group, MAX(magic_wand_size) AS `longest_magic_wand`
+FROM
+    wizzard_deposits
+GROUP BY deposit_group
+ORDER BY `longest_magic_wand` , deposit_group;
+
+#4.Smallest Deposit Group Per Magic Wand Size*
+SELECT 
+    deposit_group
+FROM
+    wizzard_deposits
+GROUP BY deposit_group
+HAVING MIN(magic_wand_size);
+
+#5.Deposits Sum
+SELECT 
+    deposit_group, SUM(deposit_amount) AS `total_sum`
+FROM
+    wizzard_deposits
+GROUP BY deposit_group
+ORDER BY `total_sum`;
+
+#6.Deposits Sum for Ollivander Family
+SELECT 
+    deposit_group, SUM(deposit_amount) AS `total_sum`
+FROM
+    wizzard_deposits
+WHERE
+    magic_wand_creator = 'Ollivander family'
+GROUP BY deposit_group
+ORDER BY deposit_group;
+
+#7.Deposits Filter
+SELECT 
+    deposit_group, SUM(deposit_amount) AS `total_sum`
+FROM
+    wizzard_deposits
+WHERE
+    magic_wand_creator = 'Ollivander family'
+GROUP BY deposit_group
+HAVING SUM(deposit_amount) < 150000
+ORDER BY `total_sum` DESC;
+
+#8.Deposit Charge
+SELECT 
+    deposit_group,
+    magic_wand_creator,
+    MIN(deposit_charge) AS `min_deposit_charge`
+FROM
+    wizzard_deposits
+GROUP BY deposit_group , magic_wand_creator
+ORDER BY magic_wand_creator , deposit_group;
+
+#9.Age Groups
+SELECT 
+    CASE
+        WHEN age <= 10 THEN '[0-10]'
+        WHEN age <= 20 THEN '[11-20]'
+        WHEN age <= 30 THEN '[21-30]'
+        WHEN age <= 40 THEN '[31-40]'
+        WHEN age <= 50 THEN '[41-50]'
+        WHEN age <= 60 THEN '[51-60]'
+        WHEN age > 60 THEN '[61+]'
+    END AS `age_group`,
+    COUNT(id) AS `wizard_count`
+FROM
+    wizzard_deposits
+GROUP BY `age_group`
+ORDER BY `age_group`;
+
+#10.First Letter
+SELECT 
+    SUBSTR(first_name, 1, 1) AS `first_letter`
+FROM
+    wizzard_deposits
+WHERE
+    deposit_group LIKE 'Troll Chest'
+GROUP BY first_name
+ORDER BY first_letter;
+
+#11.Average Interest
+SELECT 
+    deposit_group,
+    is_deposit_expired,
+    AVG(deposit_interest) AS `average_interest`
+FROM
+    wizzard_deposits
+WHERE
+    deposit_start_date > '1985-01-01'
+GROUP BY is_deposit_expired , deposit_group
+ORDER BY deposit_group DESC , is_deposit_expired;
+
+#12.Employees Minimum Salaries
+SELECT 
+    department_id, MIN(salary) AS `minimun_salary`
+FROM
+    employees
+WHERE
+    department_id IN (2 , 5, 7)
+        AND hire_date > '2000-01-01'
+GROUP BY department_id
+ORDER BY department_id;
+
+#13.Employees Average Salaries
+CREATE TABLE high_paid_employees AS SELECT * FROM
+    employees
+WHERE
+    salary > 30000;
+
+DELETE FROM high_paid_employees 
+WHERE
+    manager_id = 42;
+
+UPDATE high_paid_employees 
+SET 
+    salary = salary + 5000
+WHERE
+    department_id = 1;
+
+SELECT 
+    department_id, AVG(salary) AS `avg_salary`
+FROM
+    high_paid_employees
+GROUP BY department_id
+ORDER BY department_id;
+
+#14.Employees Maximum Salaries
+SELECT 
+    department_id, MAX(salary) AS `max_salary`
+FROM
+    employees
+GROUP BY department_id
+HAVING MAX(salary) < 30000
+    OR MAX(salary) > 70000
+ORDER BY department_id;
+
+#15.Employees Count Salaries
+SELECT 
+    COUNT(salary)
+FROM
+    employees
+WHERE
+    manager_id IS NULL;
+
+#16.3rd Highest Salary*
+SELECT 
+    emp.department_id, MAX(emp.salary) AS third_highest_salary
+FROM
+    employees AS emp
+        JOIN
+    (SELECT 
+        e.department_id AS department_id,
+            MAX(e.salary) AS max_salary
+    FROM
+        employees AS e
+    JOIN (SELECT 
+        e.department_id AS department_id,
+            MAX(e.salary) AS max_salary
+    FROM
+        employees AS e
+    GROUP BY e.department_id) AS first_max_salary ON e.department_id = first_max_salary.department_id
+    WHERE
+        e.salary < first_max_salary.max_salary
+    GROUP BY e.department_id) AS second_max_salary ON emp.department_id = second_max_salary.department_id
+WHERE
+    emp.salary < second_max_salary.max_salary
+GROUP BY emp.department_id
+ORDER BY emp.department_id;
+
+#17.Salary Challenge**
+CREATE VIEW `respect_departments` AS
+    SELECT 
+        emp.department_id AS department_id,
+        AVG(emp.salary) AS avg_salary
+    FROM
+        `employees` AS emp
+    GROUP BY emp.department_id;
+
+SELECT 
+    e.first_name,
+    e.last_name,
+    e.department_id
+FROM
+    `employees` AS e,
+    `respect_departments` AS rd
+WHERE
+    e.department_id = rd.department_id
+        AND e.salary >= rd.avg_salary
+ORDER BY e.department_id ASC
+LIMIT 10;
+
+#18.Departments Total Salaries
+SELECT 
+    department_id, SUM(salary) AS `total_salary`
+FROM
+    employees
+GROUP BY department_id
+ORDER BY department_id ASC;
+
